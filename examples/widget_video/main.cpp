946fde6906ec3e9d1deb89acb27e34027b2c348d
VaL Doroshchuk <valbok@gmail.com>
2021-09-19 13:36:31 +0200

    Introduce video widget example

---
+++ examples/widget_video/main.cpp
@@ -0,0 +1,111 @@
+/*********************************************************
+ * Copyright (C) 2020, Val Doroshchuk <valbok@gmail.com> *
+ *                                                       *
+ * This file is part of QtAVPlayer.                      *
+ * Free Qt Media Player based on FFmpeg.                 *
+ *********************************************************/
+
+#include <QtAVPlayer/qavplayer.h>
+#include <QtAVPlayer/qavvideoframe.h>
+#include <QtAVPlayer/qavaudiooutput.h>
+#include <QAbstractVideoSurface>
+#include <QVideoSurfaceFormat>
+#include <QMediaService>
+#include <QMediaObject>
+#include <QVideoWidget>
+#include <QVideoRendererControl>
+#include <QApplication>
+#include <QDebug>
+
+class VideoRenderer : public QVideoRendererControl
+{
+public:
+    QAbstractVideoSurface *surface() const override
+    {
+        return m_surface;
+    }
+
+    void setSurface(QAbstractVideoSurface *surface) override
+    {
+        m_surface = surface;
+    }
+
+    QAbstractVideoSurface *m_surface = nullptr;
+};
+
+class MediaObject;
+class MediaService : public QMediaService
+{
+public:
+    MediaService(VideoRenderer *vr, QObject* parent = nullptr)
+        : QMediaService(parent)
+        , m_renderer(vr)
+    {
+    }
+
+    QMediaControl* requestControl(const char *name) override
+    {
+        if (qstrcmp(name, QVideoRendererControl_iid) == 0)
+            return m_renderer;
+
+        return nullptr;
+    }
+
+    void releaseControl(QMediaControl *) override
+    {
+    }
+
+    VideoRenderer *m_renderer = nullptr;
+};
+
+class MediaObject : public QMediaObject
+{
+public:
+    explicit MediaObject(VideoRenderer *vr, QObject* parent = nullptr)
+        : QMediaObject(parent, new MediaService(vr, parent))
+    {
+    }
+};
+
+class VideoWidget : public QVideoWidget
+{
+public:
+    bool setMediaObject(QMediaObject *object) override
+    {
+        return QVideoWidget::setMediaObject(object);
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    VideoRenderer vr;
+
+    VideoWidget w;
+    w.show();
+
+    MediaObject mo(&vr);
+    w.setMediaObject(&mo);
+
+    QAVPlayer p;
+    QString file = argc > 1 ? QLatin1String(argv[1]) : QLatin1String("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");
+    p.setSource(QUrl(file));
+    p.play();
+
+    QAVAudioOutput audioOutput;
+    QObject::connect(&p, &QAVPlayer::audioFrame, &p, [&audioOutput](const QAVAudioFrame &frame) { audioOutput.play(frame); });
+
+    QObject::connect(&p, &QAVPlayer::videoFrame, &p, [&vr](const QAVVideoFrame &frame) {
+        if (vr.m_surface == nullptr)
+            return;
+
+        QVideoFrame videoFrame = frame.convertTo(AV_PIX_FMT_RGB32);
+        if (!vr.m_surface->isActive())
+            vr.m_surface->start({videoFrame.size(), videoFrame.pixelFormat(), videoFrame.handleType()});
+        if (vr.m_surface->isActive())
+            vr.m_surface->present(videoFrame);
+    });
+
+    return app.exec();
+}
+
