256a65946d7fb8f674a8c5ca600d3491f0c1e7e1
Val Doroshchuk <valbok@gmail.com>
2026-04-27 12:57:59 +0200

    Add isMapped to QAVVideoFrame (#584)
    It indicates that the data is mapped to CPU. For CPU based frames, it is always mapped, even if map() was not called. But useful to call isMapped on GPU based frames to check if the data has been transferred to internal CPU memory already. But calling `map()` multiple times does not produce any overhead. It will return already mapped data if any.

--- src/QtAVPlayer/qavvideoframe.cpp
+++ src/QtAVPlayer/qavvideoframe.cpp
@@ -1,9 +1,9 @@
-/*********************************************************
- * Copyright (C) 2020, Val Doroshchuk <valbok@gmail.com> *
- *                                                       *
- * This file is part of QtAVPlayer.                      *
- * Free Qt Media Player based on FFmpeg.                 *
- *********************************************************/
+/***************************************************************
+ * Copyright (C) 2020, 2026, Val Doroshchuk <valbok@gmail.com> *
+ *                                                             *
+ * This file is part of QtAVPlayer.                            *
+ * Free Qt Media Player based on FFmpeg.                       *
+ ***************************************************************/
 
 #include "qavvideoframe.h"
 #include "qavvideobuffer_cpu_p.h"
@@ -112,6 +112,12 @@ QAVVideoFrame::MapData QAVVideoFrame::map() const
 {
     Q_D(const QAVVideoFrame);
     return d->videoBuffer().map();
+}
+
+bool QAVVideoFrame::isMapped() const
+{
+    Q_D(const QAVVideoFrame);
+    return d->videoBuffer().isMapped();
 }
 
 QAVVideoFrame::HandleType QAVVideoFrame::handleType() const
