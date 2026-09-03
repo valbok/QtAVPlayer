479063ae4e17549f454a82c425648eeab5017152
Val Doroshchuk <valbok@gmail.com>
2026-09-03 14:37:16 +0200

    Update test

--- tests/auto/integration/qavplayer/tst_qavplayer.cpp
+++ tests/auto/integration/qavplayer/tst_qavplayer.cpp
@@ -1416,6 +1416,7 @@ void tst_QAVPlayer::convert()
 
     const QSize size(128, 72);
     QAVVideoFrame convertedSize = videoFrame.convertTo(to, size);
+    QVERIFY(convertedSize);
     QCOMPARE(convertedSize.format(), to);
     QCOMPARE(convertedSize.pts(), videoFrame.pts());
     QCOMPARE(convertedSize.size(), size);
